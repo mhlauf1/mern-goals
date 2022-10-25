39b10d09120decc1f0f05025f56a38822738026e
Michael <michael.laufersweiler@lulafit.com>
2022-10-25 16:30:11 -0500

    beginning of server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+
+const port = process.env.PORT || 4001;
+
+const app = express();
+
+
+app.get('/api/goals', (req, res) => {
+    res.send('Get goals');
+});
+
+app.listen(port, () => console.log(`server started on port ${port}`));
+
