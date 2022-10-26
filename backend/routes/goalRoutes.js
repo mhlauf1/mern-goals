5b8c16b7b33cfec794a25325aedf6f585b1d94e4
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook-Pro.local>
2022-10-25 23:07:47 -0500

    api with jwt authentication

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,16 +1,15 @@
 const express = require("express");
-
-const router = express.Router();
-
 const {
   getGoals,
   setGoals,
   updateGoal,
   deleteGoal,
 } = require("../controllers/goalController");
+const { protect } = require("../middleware/authMiddleware");
+const router = express.Router();
 
-router.route("/").get(getGoals).post(setGoals);
+router.route("/").get(protect, getGoals).post(protect, setGoals);
 
-router.route("/:id").put(updateGoal).delete(deleteGoal);
+router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
 
 module.exports = router;
